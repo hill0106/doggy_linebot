fe2f807e907d2bed62f801d72e962dc9f77a6946
hill0106 <hill0106trinity260935@gmail.com>
2023-01-08 02:44:04 +0800

    [fix] upload to azure.

--- yolov5.py
+++ yolov5.py
@@ -4,7 +4,7 @@ import torch
 from azure_blob import *
 
 #使用自己訓練的model best.pt來計算
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='exp14/weights/best.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True)
 def detect(path):
     #設定多少confidence以上才可以顯示出來
     if model.conf >= 0.2:
